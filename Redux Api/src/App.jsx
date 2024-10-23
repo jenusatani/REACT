c470f55c5e90b95e0c018705b0c53cf15eb64943
jenusatani <162433577+jenusatani@users.noreply.github.com>
2024-10-23 12:17:10 +0530

    Api

---
+++ Redux Api/src/App.jsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import { Provider } from 'react-redux'
+import { store } from '../App/Store'
+import ApiRedux from './Redux/ApiRedux'
+
+export default function App() {
+  return (
+    <>
+    <Provider store={store}>
+      <ApiRedux/>
+    </Provider>
+    </>
+  )
+}
